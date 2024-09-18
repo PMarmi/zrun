564eefa8fef39f0c1cf234859024754df1b836ad
NKpy07 <128097264+NKpy07@users.noreply.github.com>
2024-09-18 13:12:04 +0200

    ajust

--- execute.py
+++ execute.py
@@ -367,8 +367,8 @@ def main():
                             enemigo.vida -= resultado
 
                         if enemigo.vida <= 0:
-                            enemigos.remove(enemigo)
                             SONIDO_MUERTE.play()
+                            enemigos.remove(enemigo)
                             puntos += 5
 
                 for bala in balas:
